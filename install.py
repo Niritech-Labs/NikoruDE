899440052e7f226b6e0876cafc934d3004f9bf55
Aifi <aifi_official_ff@internet.ru>
2025-08-15 22:04:33 +0500

    Update installer
    added restore premissions

--- install.py
+++ install.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2024-2025 Niritech Labs
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 import os
 import shutil
 import subprocess
@@ -53,7 +57,30 @@ def RunProc(process,cwd = None,skiperror = False):
             print(f"state: [{R}ERR{S}]")
             exit(1)
         
+def RPrem():
+    try:
+        for froot, dirs, files in os.walk('/usr/share/NikoruDE'):
+            for dir in dirs:
+                drPath = os.path.join(froot,dir)
+                print(f'{V}Dir:{drPath} {S}')
+                os.chmod(drPath,0o755)
         
+            for file in files:
+                flPath = os.path.join(froot,file)
+                if file.endswith('.py'):
+                    print(f'{G}File exec:{flPath} {S}')
+                    os.chmod(flPath,0o755)
+                else:
+                    print(f'{V}File:{flPath} {S}')
+                    os.chmod(flPath,0o755)
+                
+        print(f"state: [{G}OK{S}]")
+    except Exception as e:
+        print(R+str(e)+S)
+        print(f"state: [{R}ERR{S}]")
+        exit(1)
+    
+
 
         
 
@@ -61,7 +88,8 @@ print(f"{B}-------------- Stage 1: Removing old App data ---------------{S}")
 Remove()
 print(f"{B}-------------- Stage 2: Copy App to device ------------------{S}")
 Copy()
-print(f"{B}-------------- Stage 3: Build Utils -------------------------{S}")
-RunProc(['sudo','/bin/python',f'{code}/modules/setup.py','build_ext',f'--build-lib=/usr/share/NikoruDE/Code/Utils'])
+print(f"{B}-------------- Stage 3: Restore premissions -------------------------{S}")
+RPrem()
 print(f"{B}-------------- Stage 4: Restore Context ---------------------{S}")
 RunProc(['sudo','restorecon','-R','-v','/usr/share/NikoruDE'],skiperror=True)
+print(f"{V}------- State: [{G}SUCCES{V}]{G} Instalation Complete{V} ----------{S}")
